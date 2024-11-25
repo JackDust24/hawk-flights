472ebeef7d69c54857b4db2d2c708fbaeb78d858
Jason Whittaker <jason.whittaker@7peakssoftware.com>
2024-11-25 12:58:03 +0700

    chore: update old files

--- server/routes/cookie-consent.js
+++ server/routes/cookie-consent.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
   const { consent } = req.body;
-
-  console.log('User consent:', consent);
   //NOTE - Would set up this in an analytics table for whether user has given consent or not
 
   res.status(200).json({ message: 'Consent recorded' });
